9b750af91319d70f178616450a494f98e6ca1204
Shihan <withanageshihan@gmail.com>
2025-04-06 18:23:13 +0530

    test adsense

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import LoginPage from '@/app/components/LoginPage';
+import Head from 'next/head';
 
 export default function Page() {
-  return <LoginPage />;
+  return( 
+    <>
+    <Head>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6339178599523298"
+        crossOrigin="anonymous"
+      ></script>
+    </Head>
+  
+  
+  <LoginPage />
+  </>
+  );
 }
